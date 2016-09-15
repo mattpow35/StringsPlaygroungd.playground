2d2a33614f6e06e1d03047a06ade48b864ac4ad0
Matt Powley <mattpow35@gmail.com>
2016-09-15 09:26:31 -0600

    added new strings that used adding comparing and interpolation

---
+++ Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var variableString = "My name is Matt."
+
+variableString += " What is your name?"
+
+variableString.insert("😊", atIndex: variableString.endIndex)
+
+
+var myName = "Matt"
+print("my name has \(myName.characters.count) characters")
+
+print("The welcome statement has \(variableString.characters.count) characters.")
+
+let String1 = "dope "
+let String2 = "cheddar"
+ var thatsCool = String1 + String2
+
+print("programming in swift is " + thatsCool + "!")
+
+let multiplier = 25
+let message = "\(multiplier) cents is in a quarter, if I have 4 quarters I will have \(Int(multiplier) * 4) cents, which is 1 dollar!💸)"
+
+
+
+
+let quote = "hello there"
+let differentquote = "hi there"
+if quote != differentquote{
+    print("the two lines above are not the same.")
+    
+}
+
+
+
+
+
+
+
+
